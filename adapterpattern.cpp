9033dc16b116ba839b812f0a01d1e4473c03b57a
xquzmee <m.chernikov@istu.edu.ua>
2023-12-24 16:17:31 +0200

    adapterpattern.cpp

---
+++ adapterpattern.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <memory>
+
+// Существующий класс с неподходящим интерфейсом
+class Adaptee {
+public:
+    void specificRequest() const {
+        std::cout << "Specific Request\n";
+    }
+};
+
+// Целевой интерфейс, который ожидается клиентом
+class Target {
+public:
+    virtual void request() const = 0;
+    virtual ~Target() {}
+};
+
+// Адаптер, который преобразует интерфейс Adaptee в Target
+class Adapter : public Target {
+public:
+    Adapter(std::unique_ptr<Adaptee> adaptee) : adaptee_(std::move(adaptee)) {}
+
+    void request() const override {
+        adaptee_->specificRequest();
+    }
+
+private:
+    std::unique_ptr<Adaptee> adaptee_;
+};
+
+int main() {
+    // Использование адаптера
+    std::unique_ptr<Adaptee> adaptee = std::make_unique<Adaptee>();
+    std::unique_ptr<Target> adapter = std::make_unique<Adapter>(std::move(adaptee));
+    adapter->request();
+
+    return 0;
+}
